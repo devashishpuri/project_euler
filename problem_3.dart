102be24013edc25abdf81db698093854690fdda0
devashishpuri <devashishpuri@gmail.com>
2020-01-08 23:17:35 +0530

    Solution 3

---
+++ problem_3.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+void main() {
+  const int numberWithFactors = 600851475143;
+
+  int largestFactor;
+  int multipleLimit = (sqrt(numberWithFactors)).round();
+
+  for (int i = 1; i <= multipleLimit; i++) {
+    if (numberWithFactors % i == 0) {
+      int largestMultiple = getLargestMultiple(i);
+      if (largestMultiple == 1) {
+        largestFactor = i;
+      }
+    }
+  }
+
+  print(largestFactor);
+}
+
+int getLargestMultiple(int number) {
+  int largestMultiple = 1;
+  int factorLimit = (sqrt(number)).round();
+  for (int i = 1; i < factorLimit; i++) {
+    if (number % i == 0) {
+      largestMultiple = i;
+    }
+  }
+  return largestMultiple;
+}
